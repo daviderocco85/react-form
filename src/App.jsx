06b23f09b6d70827f9274eb363dbb810101ced23
daviderocco85 <roccodavide85@gmail.com>
2026-07-16 17:59:14 +0200

    feat: integrate PostList and PostForm components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,27 @@ import { useState } from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import './App.css';
-
+import { PostList } from './components/PostList';
+import { PostForm } from './components/PostForm';
 
 export const App = () => {
   const [posts, setPosts] = useState(['Economia', 'Sport', 'Moda', 'Attualità', 'Cronaca', 'Benessere']);
   const [input, setInput] = useState('');
 
+
   return (
     <>
-      <ul>
-        {posts.map(post => (
-          <li key={post}>{post}<i
-            className="bi bi-trash3-fill"
-            onClick={() => setPosts(posts.filter(_post => _post !== post))} >
-          </i>
-          </li>
-        ))}
-      </ul>
-      <form onSubmit={e => {
-        e.preventDefault();
-        setPosts([...posts, input]);
-        setInput('');
-      }}>
-        <input
-          type="text"
-          value={input}
-          placeholder='Inserisci nuovo articolo'
-          onChange={e => { setInput(e.target.value) }} />
-        <button>Aggiungi</button>
-      </form>
+      <PostList
+        posts={posts}
+        onClick={(post) => setPosts(posts.filter(_post => _post !== post))} />
+      <PostForm
+        input={input}
+        onSubmit={e => {
+          e.preventDefault();
+          setPosts([...posts, input]);
+          setInput('')
+        }}
+        onChange={e => { setInput(e.target.value) }} />
     </>
-  )
+  );
 };
